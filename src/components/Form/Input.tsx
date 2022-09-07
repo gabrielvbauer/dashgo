24e273728f13a651d24afdf330a1f6f589fba670
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-09-07 10:45:50 -0300

    feat: sign in login form with react hook form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,18 +1,18 @@
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps{
   name: string;
   label?: string;
 }
 
-export function Input({name, label, ...rest}: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({name, label, ...rest}, ref) => {
   return (
     <FormControl>
       { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         name={name}
         id={name}
-        type="email"
         focusBorderColor="pink.500"
         bg="gray.900"
         variant="filled"
@@ -20,8 +20,11 @@ export function Input({name, label, ...rest}: InputProps) {
           bgColor: 'gray.900'
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
     </FormControl>
   );
 }
+
+export const Input = forwardRef(InputBase);
