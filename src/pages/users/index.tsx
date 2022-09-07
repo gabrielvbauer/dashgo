41b4e4229ba1faf21385b3fe78a76db91f53db17
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-09-07 20:31:34 -0300

    feat: pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,16 +7,18 @@ import { PageTitle } from "../../components/PageTitle";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useUsers } from "../../services/hooks/useUsers";
+import { useState } from "react";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page);
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
 
-  return (
+  return (    
     <Box>
       <Header />
 
@@ -66,7 +68,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -96,7 +98,11 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           ) }
         </Box>
