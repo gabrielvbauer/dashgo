f1cd97d248138188fecd7f0b2255658c2d1d11ca
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2022-09-07 21:29:01 -0300

    fix: user created_at field displaying as Invalid Date

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -23,11 +23,13 @@ async function getUsers(page: number): Promise<GetUsersResponse> {
   const totalCount = Number(headers['x-total-count']);
 
   const users = data.users.map(user => {
+    console.log(user)
+
     return {
       id: user.id,
       name: user.name,
       email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+      createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
         year: 'numeric'
